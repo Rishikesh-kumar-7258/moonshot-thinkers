15e0443eef194506af7ebc8c760a662812324901
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2021-09-24 20:07:40 +0530

    Made some changes

--- sub_cont/home.js
+++ sub_cont/home.js
@@ -8,51 +8,51 @@ const BOOKS = {
     "Mathematics": [
         {
             "name": "Higher Engineering Mathematics",
-            "img_src": "https://images-na.ssl-images-amazon.com/images/I/61AnHkiZtzL.jpg",
+            "img_src": "../static/images/BookImages/mathematics.jpg",
             "buy_link": "https://www.amazon.in/Higher-Engineering-Mathematics-B-S-Grewal/dp/8193328493"
         },
         {
             "name": "Higher Engineering Mathematics",
-            "img_src": "https://images-na.ssl-images-amazon.com/images/I/61AnHkiZtzL.jpg",
+            "img_src": "../static/images/BookImages/mathematics.jpg",
             "buy_link": "#"
         },
         {
             "name": "Higher Engineering Mathematics",
-            "img_src": "https://images-na.ssl-images-amazon.com/images/I/61AnHkiZtzL.jpg",
+            "img_src": "../static/images/BookImages/mathematics.jpg",
             "buy_link": "#"
         },
         {
             "name": "Higher Engineering Mathematics",
-            "img_src": "https://images-na.ssl-images-amazon.com/images/I/61AnHkiZtzL.jpg",
+            "img_src": "../static/images/BookImages/mathematics.jpg",
             "buy_link": "#"
         },
         {
             "name": "Higher Engineering Mathematics",
-            "img_src": "https://images-na.ssl-images-amazon.com/images/I/61AnHkiZtzL.jpg",
+            "img_src": "../static/images/BookImages/mathematics.jpg",
             "buy_link": "#"
         },
     ],
     "Physics": [
         {
             "name": "Higher Engineering Mathematics",
-            "img_src": "https://images-na.ssl-images-amazon.com/images/I/61AnHkiZtzL.jpg",
+            "img_src": "../static/images/BookImages/mathematics.jpg",
             "buy_link": "#"
         },
         {
             "name": "Higher Engineering Mathematics",
-            "img_src": "https://images-na.ssl-images-amazon.com/images/I/61AnHkiZtzL.jpg",
+            "img_src": "../static/images/BookImages/mathematics.jpg",
             "buy_link": "#"
         },
     ],
     "C Programming": [
         {
             "name": "Higher Engineering Mathematics",
-            "img_src": "../static/images/subject_photos/mathematics-png.jpg",
+            "img_src": "../static/images/BookImages/mathematics.jpg",
             "buy_link": "#"
         },
         {
             "name": "Higher Engineering Mathematics",
-            "img_src": "https://images-na.ssl-images-amazon.com/images/I/61AnHkiZtzL.jpg",
+            "img_src": "../static/images/BookImages/mathematics.jpg",
             "buy_link": "#"
         },
     ],
@@ -79,43 +79,41 @@ const RenderCard = (img, title, extra=null) =>
         </div>`
     )
 }
-const show_objects = 4;
+let show_objects = parseInt($(".books").width() / 200);
 let book_st = 0;
+
+window.addEventListener("resize", function(){
+    
+    show_objects = parseInt($(".books").width() / 200);
+    render_books(book_st);
+})
 
 const render_books = (book_st) =>
 {
-
-    
+    let books_section = document.querySelector(".books");
+    books_section.innerHTML = "";
     for (let i = book_st; i < book_st + show_objects; i++)
     {
         let book = BOOKS[SUBJECT_NAME][i];
-        document.querySelector('#book_section .books').innerHTML += RenderCard(book["img_src"], book["name"], book["buy_link"]);
+        books_section.innerHTML += RenderCard(book["img_src"], book["name"], book["buy_link"]);
     
     }
 }
 
-$('#book_section .left').click(() => 
+$('.left').click(() => 
 {
-    book_st += 1
+    book_st += show_objects;
+    book_st = book_st >= BOOKS[SUBJECT_NAME].length ? BOOKS[SUBJECT_NAME].length - show_objects : book_st;
+    render_books(book_st);
+})
+$('.right').click(() => 
+{
+    book_st -= show_objects;
+    book_st = (book_st < 0) ? 0 : book_st;
+    render_books(book_st);
 })
 
 render_books(book_st)
-
-// Addding tutorials to the page
-// for (let i = 0; i < 1; i++) {
-//     playlist = TUTORIALS[`${SUBJECT_NAME}`][`${i + 1}`];
-//     document.getElementById("playlist_section").innerHTML += books(playlist["img_src"], playlist["name"], playlist["link"])
-// }
-
-// Function for returning making books card
-function books(img_src, name, buy_link) {
-    let child = `<a href="${buy_link}" data-book="${name}" class="sub_book">
-                    <img src="${img_src}" alt="${name}">
-                    <h6 class="fw-bolder text-center">${name}</h6>
-                </a>`;
-
-    return child;
-}
 
 $('.sub_book').click(function () {
     sessionStorage.setItem("book", $(this).data("book"));

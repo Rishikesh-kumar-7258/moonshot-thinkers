2e3fe7abbbd3a62b1a526c785f5c181aebd4f623
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2021-09-27 22:00:31 +0530

    Added admin dashboard

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -16,6 +16,18 @@ class Books(db.Model):
     author = db.Column(db.String(200))
     desc = db.Column(db.String(500))
 
+class Hobbies(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    course_link = db.Column(db.String(200), nullable=False)
+    img = db.Column(db.String(200), nullable=False)
+    subject = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500))
+
+class Admins(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
 @app.route("/")
 def home():
@@ -30,7 +42,39 @@ def semester(num):
 def hobby():
     return render_template("hobby.html")
 
+@app.route("/admin", methods=["GET", "POST"])
+def adminpage():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        
+        user = Admins.query.filter_by(username=username).first()
+        print(user.password)
+        if (user.password == password) : return redirect("/adminloggedin")
+        else : return redirect("/admin", feedback="username of password is wrong")
+
+    return render_template("adminlogin.html")
+
+@app.route("/adminloggedin")
+def adminlogged():
+    return render_template("adminlogged.html")
+
+def adduser(username, password):
+    newuser = Admins()
+    newuser.username = username
+    newuser.password = password
+    db.session.add(newuser)
+    db.session.commit()
+
+def deleteall():
+    alladmins = Admins.query.all()
+    for admin in alladmins:
+        db.session.delete(admin)
+        db.session.commit()
+
 
 if __name__ == "__main__":
+    adduser("Prince", "Iamawesome")
+    # deleteall()
     db.create_all()
     app.run(debug=True)
